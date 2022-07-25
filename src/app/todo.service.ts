ee8f32e745be07be09c00fd29e497816684e6fb7
LokeshKumar5593 <kollurilokeshkumar@gmail.com>
2022-07-25 16:29:51 +0530

    Added Components

---
+++ src/app/todo.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  taskId:number =5;
+  todoList=[
+    {id:1,'task':"Task1"},
+    {id:2,'task':"Task2"},
+    {id:3,'task':"Task3"},
+    {id:4,'task':"Task4"}
+  ]
+  constructor() { }
+  addTodo(taskName:any){
+    this.todoList.push({'id':this.taskId++ , 'task':taskName});
+  }
+}
---
+++ src/app/todoform/todoform.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todoform',
+  templateUrl: './todoform.component.html',
+  styleUrls: ['./todoform.component.css']
+})
+export class TodoformComponent implements OnInit {
+
+  todoInput="";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  addTask(){
+return "this.i"
+  }
+  resetForm(){
+
+  }
+
+}
